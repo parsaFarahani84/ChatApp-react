b5311b08014c9520408f173db1777b927c1ca664
Parsa Frahani <90212907+parsaFarahani84@users.noreply.github.com>
2022-11-13 00:11:07 +0330

    ...

--- chatapp/chatapp/src/components/Chat.jsx
+++ chatapp/chatapp/src/components/Chat.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BsCameraVideoFill } from "react-icons/bs";
+import { IoPersonAddSharp } from "react-icons/io5";
+import { FiMoreHorizontal } from "react-icons/fi";
+import Messages from "./Messages";
 
 function Chat() {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Jane</span>
+        <div className="chatIcons">
+          <BsCameraVideoFill />
+          <IoPersonAddSharp />
+          <FiMoreHorizontal />
+        </div>
+      </div>
+      <Messages />
+    </div>
+  );
 }
 
 export default Chat;
